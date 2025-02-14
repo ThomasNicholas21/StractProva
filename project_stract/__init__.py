cd5710c826a57b3484c7bad33627e83fe88b2cdb
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-13 22:58:52 -0300

    feat: incluindo endpoints na aplicação

--- project_stract/__init__.py
+++ project_stract/__init__.py
@@ -4,6 +4,8 @@ def aplication():
     app = Flask(__name__)
 
     from project_stract.views.home_view import home_bp
+    from project_stract.views.reports_views import ads_blueprint
     app.register_blueprint(home_bp)
+    app.register_blueprint(ads_blueprint)
 
     return app
