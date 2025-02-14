cf8e8830419578ec2b1fdfb45b8abf8860b5c3a5
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-14 17:49:49 -0300

    feat: verificando valores de cada plataforma

--- project_stract/services/api_stract.py
+++ project_stract/services/api_stract.py
@@ -2,11 +2,27 @@ import requests
 
 
 # verificando platformas
-def get_platforma():
+def get_platforms():
     url = "https://sidebar.stract.to/api/platforms"
     headers = {"Authorization": "ProcessoSeletivoStract2025"}
     response = requests.get(url=url, headers=headers)
     return response.json()
 
-plataformas = get_platforma()
-print(plataformas)
+plataformas = get_platforms()
+# print(plataformas)
+
+def get_accounts(plataforma):
+    url = f"https://sidebar.stract.to/api/accounts?platform={plataforma}"
+    headers = {"Authorization": "ProcessoSeletivoStract2025"}
+    response = requests.get(url=url, headers=headers)
+    return response.json()
+
+for plataforma in plataformas['platforms']:
+    # nome descritivo
+    nome = plataforma['text']
+    # valor
+    valor = plataforma['value']
+    contas = get_accounts(valor)
+
+    print(f'{nome}: {valor}', f'{contas}', sep='\n')
+    
