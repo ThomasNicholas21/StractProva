aaf9c025a6816da4f017e88bbe33bb9e137156b7
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-14 17:21:39 -0300

    feat: verificando plataformas

--- project_stract/services/api_stract.py
+++ project_stract/services/api_stract.py
@@ -1,1 +1,12 @@
-# será feito as requisiçoes de dados
+import requests
+
+
+# verificando platformas
+def get_platforma():
+    url = "https://sidebar.stract.to/api/platforms"
+    headers = {"Authorization": "ProcessoSeletivoStract2025"}
+    response = requests.get(url=url, headers=headers)
+    return response.json()
+
+plataformas = get_platforma()
+print(plataformas)
