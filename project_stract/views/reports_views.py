ea5bc12b3e37cb5e3e8fa57fcd60ba47ad4e13a5
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-13 23:02:16 -0300

    test: testando cada endpoint criado ✔

--- project_stract/views/reports_views.py
+++ project_stract/views/reports_views.py
@@ -2,19 +2,19 @@ from flask import Blueprint
 
 ads_blueprint = Blueprint("ads", __name__)
 
-@ads_blueprint.route('/<string:platform>', methods=['GET'])
-def get_ads_by_platform():
-    return 'teste'
+@ads_blueprint.route('/<string:platforma>', methods=['GET'])
+def get_ads_by_platform(platforma):
+    return 'teste /plataforma'
 
-@ads_blueprint.route('/<string:platform>/resumo', methods=['GET'])
-def get_platform_summary():
-    return 'teste'
+@ads_blueprint.route('/<string:platforma>/resumo', methods=['GET'])
+def get_platform_summary(platforma):
+    return 'teste /plataforma/resumo'
 
 @ads_blueprint.route('/geral', methods=['GET'])
 def get_all_ads():
-    return 'teste'
+    return 'teste /geral'
 
 @ads_blueprint.route('/geral/resumo', methods=['GET'])
 def get_general_summary():
-    return 'teste'
+    return 'teste /geral/resumo'
 
