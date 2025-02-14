87464b88661fd3345434cda048a15d3bcd4e84c0
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-13 21:57:07 -0300

    feat: alterando dados para utilizar função geradora e retornar informações do endpoint "/"

--- project_stract/views/home_view.py
+++ project_stract/views/home_view.py
@@ -1,15 +1,14 @@
 from flask import Blueprint, jsonify
+from project_stract.utils.generate_csv import generator_csv
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/', methods=['GET'])
 def home():
-    dados_json = jsonify(
-        {
-            'nome':'Thomas Nicholas Pedrosa Matias',
-            'email': 'thomasnicholaas@gmail.com',
-            'link': 'https://www.linkedin.com/in/thomaas-nicholas/'
+    dados_json = {
+        'nome':'Thomas Nicholas Pedrosa Matias',
+        'email': 'thomasnicholaas@gmail.com',
+        'link': 'https://www.linkedin.com/in/thomaas-nicholas/'
         }
-    )
 
-    return dados_json
+    return generator_csv(dados_json, filename='home_data.csv')
