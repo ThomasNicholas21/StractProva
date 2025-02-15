8e20b147903fdf0c57f35210269c17c7e8c7d6db
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-14 22:19:52 -0300

    feat: fazendo requisição de accounts com todas as paginas

--- project_stract/services/api_stract.py
+++ project_stract/services/api_stract.py
@@ -12,10 +12,34 @@ plataformas = get_platforms()
 # print(plataformas)
 
 def get_accounts(plataforma):
-    url = f"https://sidebar.stract.to/api/accounts?platform={plataforma}"
+    url = f"https://sidebar.stract.to/api/accounts"
     headers = {"Authorization": "ProcessoSeletivoStract2025"}
-    response = requests.get(url=url, headers=headers)
-    return response.json()
+    
+    all_accounts_infos = []
+    page = 1 
+
+    while True:
+        params = {"platform": plataforma, "page": page}
+        response = requests.get(url=url, headers=headers, params=params)
+        
+        if response.status_code != 200:
+            print(f"Erro ao buscar dados: {response.status_code}")
+            break
+
+        data = response.json()
+
+        all_accounts_infos.extend(data.get("accounts", []))
+
+        pagination = data.get("pagination", {})
+        current_page = pagination.get("current", 1)
+        total_pages = pagination.get("total", 1)
+
+        if current_page >= total_pages:
+            break
+
+        page += 1
+
+    return all_accounts_infos
 
 for plataforma in plataformas['platforms']:
     # nome descritivo
@@ -25,4 +49,4 @@ for plataforma in plataformas['platforms']:
     contas = get_accounts(valor)
 
     print(f'{nome}: {valor}', f'{contas}', sep='\n')
-    
+
