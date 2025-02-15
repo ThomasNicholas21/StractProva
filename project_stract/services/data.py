5edf646413a39bdfdb4946a6eff9a32edbfff327
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-15 15:03:38 -0300

    feat: pegando chave text da plataforma

--- project_stract/services/data.py
+++ project_stract/services/data.py
@@ -1,4 +1,4 @@
-from api_stract import get_platforms, get_accounts, get_fields, get_insights
+from project_stract.services.api_stract import get_platforms, get_accounts, get_fields, get_insights
 
 # O endpoint "/{{plataforma}}" deve retornar uma 
 # tabela em que cada linha represente um anúncio 
@@ -7,44 +7,25 @@ from api_stract import get_platforms, get_accounts, get_fields, get_insights
 # anúncio, bem como o nome da conta que o está veiculando. 
 # Em nenhuma das tabelas precisam ser retornados IDs, 
 # mas é importante observar que o nome não é um identificador único
-# plataformas = get_platforms()
-# plataforma1 = plataformas['platforms'][0]['value']
-# accounts = get_accounts(plataforma1)
-# fields = get_fields(plataforma1)
-# insights1 = get_insights(plataforma1, accounts[0]['id'], accounts[0]['token'], fields)
-
-    
-
-# dict_insights = {'platform': plataformas['platforms'][0]['text'], 'account_name': accounts[0]['name']}
-
-# def test(dict_insight: dict):
-#     for iterable in insights1['insights']:
-#         for chave, valor in iterable.items():
-#             dict_insight.setdefault(chave, valor)
-    
-#     return dict_insight
-
-# lista = test(dict_insights)
-# print(lista)
-
-plataformas = get_platforms()
-plataforma1 = plataformas['platforms'][0]['value']
-# plataforma2 = plataformas['platforms'][1]['value']
-# plataforma3 = plataformas['platforms'][2]['value']
-# print(plataforma1, plataforma2, plataforma3, sep='\n')
 
 # gera dados de insight para plataforma
 def generate_platform_insights(plataforma):
     accounts = get_accounts(plataforma)
     fields = get_fields(plataforma)
+    text_platforma = ''
 
     all_insights = []
+
+    plataformas = get_platforms()
+    for value_i in plataformas['platforms']:
+        if value_i['value'] == plataforma:
+            text_platforma = value_i['text']
 
     for account in accounts:
         account_name = account['name']
         account_id = account['id']
         account_token = account['token']
-        dict_default = {'platform': plataforma, 'account_name': account_name}
+        dict_default = {'platform': text_platforma, 'account_name': account_name}
 
         insights_format = get_insights(plataforma, account_id, account_token, fields)
 
@@ -58,10 +39,9 @@ def generate_platform_insights(plataforma):
     
     return all_insights
 
-teste = generate_platform_insights(plataforma1)
-print(teste)
+if __name__ == "__main__":
+    plataformas = get_platforms()
+    plataforma1 = plataformas['platforms'][0]['value']
+    teste = generate_platform_insights(plataforma1)
 
-
-# dados = generate_platform_insights(plataforma1)
-# print(dados)
-# generate_platform_insights(plataforma1)
+    print(teste)
