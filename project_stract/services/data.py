d329df8bb441db5bcad3114ebc8556ce8dc70f81
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-15 20:44:34 -0300

    chore: organizando módulo

--- project_stract/services/data.py
+++ project_stract/services/data.py
@@ -1,13 +1,5 @@
 from project_stract.services.api_stract import get_platforms, get_accounts, get_fields, get_insights
-# from api_stract import get_platforms, get_accounts, get_fields, get_insights
 
-# O endpoint "/{{plataforma}}" deve retornar uma 
-# tabela em que cada linha represente um anúncio 
-# veiculado na plataforma indicada. As colunas 
-# devem trazer todos os campos de insights daquele 
-# anúncio, bem como o nome da conta que o está veiculando. 
-# Em nenhuma das tabelas precisam ser retornados IDs, 
-# mas é importante observar que o nome não é um identificador único
 
 # gera dados de insight para plataforma
 def generate_platform_insights(plataforma):
@@ -41,16 +33,10 @@ def generate_platform_insights(plataforma):
     return all_insights
 
 
-# O endpoint "/{{plataforma}}/resumo" deve trazer uma tabela similar, 
-# mas colapsando em uma única linha todas as linhas que forem da mesma 
-# conta, ficando apenas uma linha para cada conta. Os dados devem ser 
-# somados nas colunas numéricas e, nas colunas que tem texto, os dados 
-# podem ficar vazios (exceto o nome da conta, que é o mesmo para todas 
-# as linhas agregadas da conta em questão). 
+# gera dados de insight para plataforma de forma resumida
 def generate_platform_insights_summary(plataforma):
     all_insights = generate_platform_insights(plataforma)
     
-    # Dicionário para armazenar a soma dos valores
     summary_insights = {}
     summary_insights_list = []
 
@@ -70,21 +56,12 @@ def generate_platform_insights_summary(plataforma):
     return summary_insights_list
 
 
-# O endpoint "/geral" deve trazer todos os anúncios de todas as plataformas. 
-# Nesse relatório devem ser adicionadas colunas para identificar por nome a 
-# plataforma na qual o anúncio está sendo veiculado, além do nome conta que 
-# esta veiculando o anúncio. Devem haver colunas para todos os campos existentes 
-# na API. Campos de diferentes plataformas que possuam o mesmo nome podem ser 
-# apresentados na mesma coluna, isto é, uma mesma coluna pode (mas não necessariamente 
-# deve) ser aproveitada pelas mesmas plataformas. O campo pode ficar vazio nas linhas 
-# das plataformas nas quais ele não exista. Uma exceção é o campo "Cost per Click", 
-# que não está disponível para esta API no Google Analytics, porém pode ser calculado 
-# dividindo-se o valor de "spend" pelo valor de "clicks".
+# gera dados de insight de todas as plataformas.
 def generate_all_platform_insights():
     platforms = get_platforms()
     list_all_platforms = []
 
-    # Coletar todos os insights
+
     for platform in platforms['platforms']:
         for key_platform, value_platform in platform.items():
             if key_platform == 'value':
@@ -115,10 +92,8 @@ def generate_all_platform_insights():
 
     return list_all_platforms
 
-# O endpoint "/geral/resumo" deve trazer uma tabela similar, mas colapsando em uma única linha 
-# todas as linhas que forem da mesma plataforma. Caso as colunas seja numéricas, os dados devem 
-# ser somados. Caso sejam texto, a coluna pode ficar vazia na linha em questão (exceto nome da 
-# plataforma, que é o mesmo para todas as linhas agregadas da plataforma em questão).
+
+# gera dados de insight de todas as plataformas de forma resumida.
 def generate_all_platform_insights_summary():
     list_all_platforms = generate_all_platform_insights()
 
@@ -155,10 +130,3 @@ def generate_all_platform_insights_summary():
                 summary_insights[key] = ""
 
     return list(summary_insights_dict.values())
-
-
-
-if __name__ == "__main__":
-
-    valores = generate_all_platform_insights()
-    print(valores)
