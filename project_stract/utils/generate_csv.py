85403d6e2656e89e350c1d78bac8b03bf2339389
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-15 15:18:33 -0300

    feat: atribuindo enconding utf-8

--- project_stract/utils/generate_csv.py
+++ project_stract/utils/generate_csv.py
@@ -6,7 +6,7 @@ def generator_csv(data, *, filename):
     if not data:
         return Response('No data', mimetype='text/csv')
 
-    output = StringIO()
+    output = StringIO(newline='')
 
     if isinstance(data, dict):
         data = [data]
@@ -18,7 +18,8 @@ def generator_csv(data, *, filename):
     writer.writeheader()
     writer.writerows(data)
 
-    response = Response(output.getvalue(), mimetype="text/csv")
+    response = Response(output.getvalue().encode('utf-8-sig'), mimetype="text/csv")
     response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    response.headers["Content-Type"] = "text/csv; charset=utf-8"
     
     return response
