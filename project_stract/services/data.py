a981bb65c7f09ca88b2a7298aa43c81755e1f0f7
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-15 14:08:17 -0300

    test: montando valores do endpoint platforms

--- project_stract/services/data.py
+++ project_stract/services/data.py
@@ -9,6 +9,25 @@ from api_stract import get_platforms, get_accounts, get_fields, get_insights
 # mas é importante observar que o nome não é um identificador único
 plataformas = get_platforms()
 plataforma1 = plataformas['platforms'][0]['value']
+accounts = get_accounts(plataforma1)
+fields = get_fields(plataforma1)
+insights1 = get_insights(plataforma1, accounts[0]['id'], accounts[0]['token'], fields)
+
+    
+
+dict_insights = {'platform': plataformas['platforms'][0]['text'], 'account_name': accounts[0]['name']}
+
+def test(dict_insight: dict):
+    for iterable in insights1['insights']:
+        for chave, valor in iterable.items():
+            dict_insight.setdefault(chave, valor)
+    
+    return dict_insight
+
+lista = test(dict_insights)
+print(lista)
+
+# print(plataforma1, accounts, fields, insights, sep='\n\n')
 # plataforma2 = plataformas['platforms'][1]['value']
 # plataforma3 = plataformas['platforms'][2]['value']
 # print(plataforma1, plataforma2, plataforma3, sep='\n')
@@ -32,13 +51,13 @@ def generate_platform_insights(plataforma):
         insights.setdefault("account_name", account_name)
         insights.setdefault("platform", plataforma)
 
-        all_insights.append(insights)
+        all_insights.append(insights.values())
     
     return all_insights
 
 
 
 
-dados = generate_platform_insights(plataforma1)
-print(dados)
+# dados = generate_platform_insights(plataforma1)
+# print(dados)
 # generate_platform_insights(plataforma1)
