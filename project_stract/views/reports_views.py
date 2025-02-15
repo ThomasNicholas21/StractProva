7ff42d775723ee3072750d820968bd0d064cc47c
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-15 18:27:00 -0300

    feat: criando endpoint "/{{plataforma}}/resumo"

--- project_stract/views/reports_views.py
+++ project_stract/views/reports_views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from project_stract.services.data import generate_platform_insights
+from project_stract.services.data import generate_platform_insights, generate_platform_insights_summary
 from project_stract.utils.generate_csv import generator_csv
 
 ads_blueprint = Blueprint("ads", __name__)
@@ -11,9 +11,12 @@ def get_ads_by_platform(platforma):
 
     return generator_csv(get_platform_infos, filename=f'{platforma}_infos.csv')
 
-@ads_blueprint.route('/<string:platforma>/resumo', methods=['GET'])
-def get_platform_summary(platforma):
-    return 'teste /plataforma/resumo'
+@ads_blueprint.route('/<string:plataforma>/resumo', methods=['GET'])
+def get_platform_summary(plataforma):
+
+    get_platform_summary_infos = generate_platform_insights_summary(plataforma)
+
+    return generator_csv(get_platform_summary_infos, filename=f'{plataforma}_resumo.csv')
 
 @ads_blueprint.route('/geral', methods=['GET'])
 def get_all_ads():
