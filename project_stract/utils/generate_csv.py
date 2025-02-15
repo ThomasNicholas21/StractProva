1e5ebbbf6cd36515f43fd0d1254f00064830766a
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-15 14:06:47 -0300

    refactor: melhorando função para receber listas e dicionarios

--- project_stract/utils/generate_csv.py
+++ project_stract/utils/generate_csv.py
@@ -2,17 +2,23 @@ import csv
 from io import StringIO
 from flask import Response
 
-def generator_csv(data, *,filename):
+def generator_csv(data, *, filename):
     if not data:
         return Response('No data', mimetype='text/csv')
-    
+
     output = StringIO()
-    writer = csv.DictWriter(output, fieldnames=data.keys())
+
+    if isinstance(data, dict):
+        data = [data]
+
+
+    keys = data[0].keys()
+    writer = csv.DictWriter(output, fieldnames=keys)
 
     writer.writeheader()
-    writer.writerow(data)
+    writer.writerows(data)
 
     response = Response(output.getvalue(), mimetype="text/csv")
     response.headers["Content-Disposition"] = f"attachment; filename={filename}"
-
+    
     return response
