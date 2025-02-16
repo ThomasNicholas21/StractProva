a2b88a8700ee2d5023c95452f599ec18e1482208
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-15 22:20:37 -0300

    feat: criando tratamento de erro

--- project_stract/views/reports_views.py
+++ project_stract/views/reports_views.py
@@ -1,19 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from project_stract.services.data import generate_platform_insights, generate_platform_insights_summary
 from project_stract.services.data import generate_all_platform_insights, generate_all_platform_insights_summary
 from project_stract.utils.generate_csv import generator_csv
 
 ads_blueprint = Blueprint("ads", __name__)
 
+VALID_PARAMS_PLATFORM = ['meta_ads', 'ga4', 'tiktok_insights']
+
 @ads_blueprint.route('/<string:platforma>', methods=['GET'])
-def get_ads_by_platform(platforma):
+def get_ads_by_platform(plataforma):
+    if plataforma not in VALID_PARAMS_PLATFORM:
+        return jsonify({"error": "Plataforma invalida"}), 400
 
-    get_platform_infos = generate_platform_insights(platforma)
+    get_platform_infos = generate_platform_insights(plataforma)
 
-    return generator_csv(get_platform_infos, filename=f'{platforma}_infos.csv')
+    return generator_csv(get_platform_infos, filename=f'{plataforma}_infos.csv')
 
 @ads_blueprint.route('/<string:plataforma>/resumo', methods=['GET'])
 def get_platform_summary(plataforma):
+    if plataforma not in VALID_PARAMS_PLATFORM:
+        return jsonify({"error": "Plataforma invalida"}), 400
 
     get_platform_summary_infos = generate_platform_insights_summary(plataforma)
 
